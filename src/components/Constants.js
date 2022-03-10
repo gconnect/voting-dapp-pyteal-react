184b38ad80fe7e1cc927d3f6901a8257fcbb63e1
Glory Agatevure <agatevureglory@gmail.com>
2022-03-10 14:49:27 +0100

    Changed app id

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -4,5 +4,5 @@ export const CONSTANTS = {
    algodToken : {"X-API-Key": "fetqTyZ8r82MSX9YT2pLq53iRMZwVibQx3TtrZ2h"},
    baseServer : 'https://testnet-algorand.api.purestake.io/ps2/',
    port : '',
-   APP_ID : 76927698
+   APP_ID : 76971672
 }
